21697fe1a0f3500f52189e39110142d70c567e94
FuthifoXx <futhifoxmaseko@gmail.com>
2024-08-10 13:12:12 +0200

    first commit

---
+++ js/cript.js
@@ -0,0 +1,18 @@
+'strict';
+//Sum of positive
+//You get an array of numbers, return the sum of all the positive ones.
+
+// function positiveSum(arr) {
+//   return arr.filter((a) => a >= 0).reduce((a,b)=>{return a + b},0)
+// }
+
+const positiveSum = (arr) => arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
+
+// console.log(positiveSum([1, 2, 3, 4]));
+/////////////////////////////////////////////////////
+
+const exponential = (numbers) =>
+  numbers.reduce((a, b) => {
+    return a + Math.pow(b, 2);
+  }, 0);
+console.log(exponential([1, 2, 3, 4]));
