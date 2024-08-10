e93011bfc8f512e51c91590ed423e653e69a9694
FuthifoXx <futhifoxmaseko@gmail.com>
2024-08-10 13:39:51 +0200

    another code added

--- js/cript.js
+++ js/cript.js
@@ -10,9 +10,15 @@ const positiveSum = (arr) => arr.reduce((a, b) => a + (b > 0 ? b : 0), 0);
 
 // console.log(positiveSum([1, 2, 3, 4]));
 /////////////////////////////////////////////////////
-
+//Exponential
 const exponential = (numbers) =>
   numbers.reduce((a, b) => {
     return a + Math.pow(b, 2);
   }, 0);
 console.log(exponential([1, 2, 3, 4]));
+
+function squareSum(numbers) {
+  return numbers.reduce(function (sum, n) {
+    return n * n + sum;
+  }, 0);
+}
